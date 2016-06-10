67f68246f488a24c428c48200c7d3b748449ad85
ryannewington <ryan@lithiumblue.com>
2016-06-10 12:03:37 +1000

    Adds a new Clear-FullSyncWarning cmdlet

---
+++ src/Lithnet.Miiserver.Automation/Clear-FullSyncWarning.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Management.Automation;
+using Lithnet.Miiserver.Client;
+
+namespace Lithnet.Miiserver.Automation
+{
+    [Cmdlet(VerbsCommon.Clear, "FullSyncWarning")]
+    public class ClearFullSyncWarning : MATargetCmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            this.MAInstance.SuppressFullSyncWarning();
+        }
+    }
+}
