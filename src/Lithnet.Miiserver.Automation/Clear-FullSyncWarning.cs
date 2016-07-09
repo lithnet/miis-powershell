a9735d172a890eb266150d8caeb386de44baf347
ryannewington <ryan@lithiumblue.com>
2016-07-09 10:48:05 +1000

    Modifies the Clear-FullSyncWarning cmdlet to accept pipeline input, and also allow parameterless invocation to clear the full sync warning from all management agents

--- src/Lithnet.Miiserver.Automation/Clear-FullSyncWarning.cs
+++ src/Lithnet.Miiserver.Automation/Clear-FullSyncWarning.cs
@@ -7,12 +7,55 @@ using Lithnet.Miiserver.Client;
 
 namespace Lithnet.Miiserver.Automation
 {
-    [Cmdlet(VerbsCommon.Clear, "FullSyncWarning")]
-    public class ClearFullSyncWarning : MATargetCmdlet
+    [Cmdlet(VerbsCommon.Clear, "FullSyncWarning", DefaultParameterSetName = "none")]
+    public class ClearFullSyncWarning : Cmdlet
     {
+        private ManagementAgent ma;
+
+        [Parameter(Mandatory = false, Position = 1, ValueFromPipeline = false, ParameterSetName = "MAName"), ValidateNotNullOrEmpty]
+        public string MAName { get; set; }
+
+        [Parameter(Mandatory = false, Position = 1, ValueFromPipeline = true, ParameterSetName = "MAObject"), ValidateNotNullOrEmpty]
+        public ManagementAgent MA { get; set; }
+
+        protected ManagementAgent MAInstance
+        {
+            get
+            {
+                if (this.MA != null)
+                {
+                    return this.MA;
+                }
+                else if (this.MAName != null)
+                {
+                    if (this.ma == null)
+                    {
+                        this.ma = MiisController.GetManagementAgent(this.MAName, false);
+                    }
+
+                    return this.ma;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
         protected override void ProcessRecord()
         {
-            this.MAInstance.SuppressFullSyncWarning();
+            ManagementAgent instance = this.MAInstance;
+
+            if (instance == null)
+            {
+                foreach (ManagementAgent ma in MiisController.GetManagementAgents())
+                {
+                    ma.SuppressFullSyncWarning();
+                }
+            }
+            else
+            {
+                instance.SuppressFullSyncWarning();
+            }
         }
     }
 }
